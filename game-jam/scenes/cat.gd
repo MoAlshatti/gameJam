159e9dfbddf10ac09ab04c2ad63abb6a8f3a2e2f
Sajjad <saajad914@hotmail.com>
2025-11-15 19:55:51 +0000

    minor

--- game-jam/scenes/cat.gd
+++ game-jam/scenes/cat.gd
@@ -10,35 +10,35 @@ const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
-	animated_sprite.play("idle")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		animated_sprite.play("jumping")
-		
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY  
+	else: 
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY  
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	
-
-		
-	if velocity.x > 0:
-		animated_sprite.flip_h = true
-	elif velocity.x < 0:
-		animated_sprite.flip_h = false
-		
-	if velocity.x != 0 && animated_sprite.animation == "idle":
-		animated_sprite.play("walking")
-		
-
-	if direction:
-		velocity.x = direction * SPEED
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("left", "right")
 			
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.x > 0:
+			animated_sprite.flip_h = true
+		elif velocity.x < 0:
+			animated_sprite.flip_h = false
+			
+		if velocity.x != 0:
+			animated_sprite.play("walking")
+		else:			
+			animated_sprite.play("idle")
+			
+
+		if direction:
+			velocity.x = direction * SPEED
+				
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
